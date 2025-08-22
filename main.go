afd278ca4875bd2f887cd942298e2cba9de6bff0
bishalr0y <bishalroy895@gmail.com>
2025-08-22 11:57:12 +0530

    feat: add args

--- main.go
+++ main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 )
 
-func main() {
-	filename := "config"
-	username := "bishalr0y"
-	branch := "main"
-	repo := "dotfiles"
-	path := ".config/ghostty/config"
+// curl -o filename.ext https://raw.githubusercontent.com/username/repo/branch/path/to/file.ext
 
-	url := fmt.Sprintf(
-		"https://raw.githubusercontent.com/%v/%v/%v/%v",
-		username,
-		repo,
-		branch,
-		path,
-	)
+func main() {
+	args := os.Args
+
+	if len(args) == 1 || args[1] == "help" {
+		fmt.Printf("Usage:\n\nghdl output_location github_url\n")
+		return
+	}
+
+	urlSlice := strings.Split(args[2], "/")
+	filename := urlSlice[len(urlSlice)-1]
+
+	url := args[2]
 
 	cmd := exec.Command("curl", "-o", filename, url)
 
